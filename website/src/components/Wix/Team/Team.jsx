324aafdc62021a3a905a1dc4158138a6d9d8742c
24ERIC <60751655+24ERIC@users.noreply.github.com>
2023-02-25 15:58:30 -0500

    preset up for team component

---
+++ website/src/components/Wix/Team/Team.jsx
@@ -0,0 +1,27 @@
+import Team_1 from "./Team_1/Team_1";
+import Team_2 from "./Team_2/Team_2";
+
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+
+
+export default function Team() {
+
+    return (
+      <>
+        <Typography variant="h2" textAlign={"center"}> Components: Team</Typography>
+        <Button component={Link} to="/" color="primary">Home</Button>
+
+        <Typography variant="h4" textAlign={"center"} marginBottom={10} marginTop={5}>Below is Team_1</Typography>
+        <Welcome_1 />
+
+        <Typography variant="h4" textAlign={"center"} marginBottom={10} marginTop={10}>Below is Team_2</Typography>
+        <Welcome_2 />
+
+        <Typography variant="h4" textAlign={"center"} marginBottom={10} marginTop={10}>Below is Team_3</Typography>
+        <Welcome_3 />
+      </>
+    );
+  }
+    
