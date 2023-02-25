b5e8fe56bb6859291144781c528714552d7c5871
24ERIC <60751655+24ERIC@users.noreply.github.com>
2023-02-24 20:37:01 -0500

    Update Home.jsx

--- website/src/components/Home.jsx
+++ website/src/components/Home.jsx
@@ -7,30 +7,34 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Home() {
-  <Hi />
+  
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            Photos
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            Photos
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <>
+      <h1>Who lives in my Garage?</h1>
+      <Hi />
+    </>
+    // <Box sx={{ flexGrow: 1 }}>
+    //   <AppBar position="static">
+    //     <Toolbar variant="dense">
+    //       <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+    //         <MenuIcon />
+    //       </IconButton>
+    //       <Typography variant="h6" color="inherit" component="div">
+    //         Photos
+    //       </Typography>
+    //     </Toolbar>
+    //   </AppBar>
+    //   <AppBar position="static">
+    //     <Toolbar variant="dense">
+    //       <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+    //         <MenuIcon />
+    //       </IconButton>
+    //       <Typography variant="h6" color="inherit" component="div">
+    //         Photos
+    //       </Typography>
+    //     </Toolbar>
+    //   </AppBar>
+    // </Box>
     
   );
 }
