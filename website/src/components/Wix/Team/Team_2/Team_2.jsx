c386682913df4b7f61c375af7c92c725c2dc0fa2
24ERIC <60751655+24ERIC@users.noreply.github.com>
2023-02-26 14:15:33 -0500

    complete team 2, complete team

--- website/src/components/Wix/Team/Team_2/Team_2.jsx
+++ website/src/components/Wix/Team/Team_2/Team_2.jsx
@@ -20,30 +20,50 @@ export default function Team_2() {
     <>
       <Introduction />
        
-      <Grid
-        marginTop={"15%"}
-        container
-        justify="center"
-      >
+      <Grid marginTop={"15%"} container justify="center">
         <Grid item xs={3} sm={3} md={3}>
           <TeamMember1Picture />
         </Grid>
         <Grid item xs={3} sm={3} md={3}>
-          <TeamMember11 />
+          <TeamMember1Text />
         </Grid>
 
-        {/* <Grid item xs={12} sm={4} md={6}>
-          <TeamMember2 />
+        
+        <Grid item xs={3} sm={3} md={3}>
+          <TeamMember2Picture />
         </Grid>
-         <Grid item xs={12} sm={6} md={6}>
-          <TeamMember3 />
+        <Grid item xs={3} sm={3} md={3}>
+          <TeamMember2Text />
         </Grid>
-        <Grid item xs={12} sm={6} md={6}>
-          <TeamMember4 />
+
+        <Grid item xs={3} sm={3} md={3}>
+          <TeamMember3Picture />
         </Grid>
-        <Grid item xs={12} sm={6} md={6}>
-          <TeamMember5 />
-        </Grid> */}
+        <Grid item xs={3} sm={3} md={3}>
+          <TeamMember3Text />
+        </Grid>
+
+        <Grid item xs={3} sm={3} md={3}>
+          <TeamMember4Picture />
+        </Grid>
+        <Grid item xs={3} sm={3} md={3}>
+          <TeamMember4Text />
+        </Grid>
+
+        <Grid item xs={3} sm={3} md={3}>
+          <TeamMember5Picture />
+        </Grid>
+        <Grid item xs={3} sm={3} md={3}>
+          <TeamMember5Text />
+        </Grid>
+
+        <Grid item xs={3} sm={3} md={3}>
+          <TeamMember6Picture />
+        </Grid>
+        <Grid item xs={3} sm={3} md={3}>
+          <TeamMember6Text />
+        </Grid>
+
       </Grid>
     </>
   );
@@ -85,17 +105,227 @@ function Introduction() {
 }
 
 
-
-
-
-
-
-
-
-
-
-
 function TeamMember1Picture() {
+
+  return (
+    <Card 
+      sc = {{
+        display: 'flex',
+        // maxWidth: 600,
+      }}
+    >
+      <CardMedia
+        component="img"
+        image={image1}
+        // width="600"
+      />
+    </Card>
+  );
+}
+
+
+function TeamMember1Text() {
+
+  return (
+    <Card 
+      sc = {{
+        display: 'flex',
+        maxWidth: 600,
+      }}
+    >
+      <CardContent>
+        <br />
+        <Typography variant="h6" component="h2">
+          Co-Founder & Partner
+        </Typography>
+        <Typography variant="h6" component="h2" fontWeight={"800"}>
+          Taylor Quill
+        </Typography>
+        <br />
+        <br />
+        <br />
+        <Typography variant="subtitle" component="p">
+          info@mysite.com
+        </Typography>
+        <br />
+        <Typography variant="body3" component="p">
+          123-456-7890
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+
+
+
+
+function TeamMember2Picture() {
+
+  return (
+    <Card 
+      sc = {{
+        display: 'flex',
+        // maxWidth: 600,
+      }}
+    >
+      <CardMedia
+        component="img"
+        image={image2}
+        // width="600"
+      />
+    </Card>
+  );
+}
+
+
+function TeamMember2Text() {
+
+  return (
+    <Card 
+      sc = {{
+        display: 'flex',
+        maxWidth: 600,
+      }}
+    >
+      <CardContent>
+        <br />
+        <Typography variant="h6" component="h2">
+          Co-Founder & Partner
+        </Typography>
+        <Typography variant="h6" component="h2" fontWeight={"800"}>
+          Taylor Quill
+        </Typography>
+        <br />
+        <br />
+        <br />
+        <Typography variant="subtitle" component="p">
+          info@mysite.com
+        </Typography>
+        <br />
+        <Typography variant="body3" component="p">
+          123-456-7890
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+
+
+
+
+function TeamMember3Picture() {
+
+  return (
+    <Card 
+      sc = {{
+        display: 'flex',
+        // maxWidth: 600,
+      }}
+    >
+      <CardMedia
+        component="img"
+        image={image3}
+        // width="600"
+      />
+    </Card>
+  );
+}
+
+
+function TeamMember3Text() {
+
+  return (
+    <Card 
+      sc = {{
+        display: 'flex',
+        maxWidth: 600,
+      }}
+    >
+      <CardContent>
+        <br />
+        <Typography variant="h6" component="h2">
+          Co-Founder & Partner
+        </Typography>
+        <Typography variant="h6" component="h2" fontWeight={"800"}>
+          Taylor Quill
+        </Typography>
+        <br />
+        <br />
+        <br />
+        <Typography variant="subtitle" component="p">
+          info@mysite.com
+        </Typography>
+        <br />
+        <Typography variant="body3" component="p">
+          123-456-7890
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+
+
+
+
+function TeamMember4Picture() {
+
+  return (
+    <Card 
+      sc = {{
+        display: 'flex',
+        // maxWidth: 600,
+      }}
+    >
+      <CardMedia
+        component="img"
+        image={image4}
+        // width="600"
+      />
+    </Card>
+  );
+}
+
+
+function TeamMember4Text() {
+
+  return (
+    <Card 
+      sc = {{
+        display: 'flex',
+        maxWidth: 600,
+      }}
+    >
+      <CardContent>
+        <br />
+        <Typography variant="h6" component="h2">
+          Co-Founder & Partner
+        </Typography>
+        <Typography variant="h6" component="h2" fontWeight={"800"}>
+          Taylor Quill
+        </Typography>
+        <br />
+        <br />
+        <br />
+        <Typography variant="subtitle" component="p">
+          info@mysite.com
+        </Typography>
+        <br />
+        <Typography variant="body3" component="p">
+          123-456-7890
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+
+
+
+
+function TeamMember5Picture() {
 
   return (
     <Card 
@@ -112,7 +342,9 @@ function TeamMember1Picture() {
     </Card>
   );
 }
-function TeamMember1Text() {
+
+
+function TeamMember5Text() {
 
   return (
     <Card 
@@ -122,12 +354,22 @@ function TeamMember1Text() {
       }}
     >
       <CardContent>
-        <Typography variant="h5" component="h2">
-          Card Title
+        <br />
+        <Typography variant="h6" component="h2">
+          Co-Founder & Partner
         </Typography>
-        <Typography variant="body2" component="p">
-          This is the text for the card. It can be multiple lines long and contain
-          any content you want to display.
+        <Typography variant="h6" component="h2" fontWeight={"800"}>
+          Taylor Quill
+        </Typography>
+        <br />
+        <br />
+        <br />
+        <Typography variant="subtitle" component="p">
+          info@mysite.com
+        </Typography>
+        <br />
+        <Typography variant="body3" component="p">
+          123-456-7890
         </Typography>
       </CardContent>
     </Card>
@@ -139,197 +381,56 @@ function TeamMember1Text() {
 
 
 
+function TeamMember6Picture() {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function TeamMember1() {
-//   return (
-//     <Card
-//       sx={{
-//         position: 'relative',
-//       }}
-//     >
-//       {/* <CardContent
-//         sx={{
-//           position: 'absolute',
-//           bottom: "-9%",
-//           left: "20%",
-//           width: '100',
-//           height: '30',
-//           backgroundColor: 'white',
-          
-//           // display: "flex",
-//           // flexDirection: "column",
-//           // justifyContent: "center",
-//           // alignItems: "center",
-//         }}
-//       >
-//         <Typography variant="h4" marginBottom={"10%"}>Molaen Neak</Typography>
-
-//       </CardContent> */}
-//       <CardContent
-//         sx={{
-//           position: 'absolute',
-//           bottom: "20%",
-//           left: "30%",
-//           width: '40%',
-//           height: '65%',
-//           backgroundColor: 'white',
-          
-//           display: "flex",
-//           flexDirection: "column",
-//           justifyContent: "center",
-//           alignItems: "center",
-//         }}
-//       >
-//         <Typography variant="h4" marginBottom={"10%"}>Lizard</Typography>
-//         <Typography variant="caption" width={"70%"} marginBottom={"10%"}>Welcome. We're efv, and we're disrupting the industry with our groundbreaking solutions and business ideas. Our technology has already been recognized by market leaders and our scalable business model is setting the trend for industry standards. Curious to find out more? Browse through our website, and get in touch if you would like to request a demo.</Typography>
-//         <Button variant="contained"><Typography variant="caption" >Learn More</Typography></Button>
-//       </CardContent>
-//       <CardMedia component="img" image={image1} />
-//     </Card>
-//   );
-// }
-
-
-
-function TeamMember2() {
   return (
-    <Card
-      sx={{
-        position: 'relative',
+    <Card 
+      sc = {{
+        display: 'flex',
+        // maxWidth: 600,
       }}
     >
-      <CardContent
-        sx={{
-          position: 'absolute',
-          bottom: "-9%",
-          left: "20%",
-          width: '100',
-          height: '30',
-          backgroundColor: 'white',
-          
-          // display: "flex",
-          // flexDirection: "column",
-          // justifyContent: "center",
-          // alignItems: "center",
-        }}
-      >
-        <Typography variant="h4" marginBottom={"10%"}>Molaen Neak</Typography>
-
-      </CardContent>
-      <CardMedia component="img" image={image2} />
+      <CardMedia
+        component="img"
+        image={image6}
+        // width="600"
+      />
     </Card>
   );
 }
 
 
+function TeamMember6Text() {
 
-function TeamMember3() {
   return (
-    <Card
-      sx={{
-        position: 'relative',
+    <Card 
+      sc = {{
+        display: 'flex',
+        maxWidth: 600,
       }}
     >
-      <CardContent
-        sx={{
-          position: 'absolute',
-          bottom: "-9%",
-          left: "20%",
-          width: '100',
-          height: '30',
-          backgroundColor: 'white',
-          
-          // display: "flex",
-          // flexDirection: "column",
-          // justifyContent: "center",
-          // alignItems: "center",
-        }}
-      >
-        <Typography variant="h4" marginBottom={"10%"}>Molaen Neak</Typography>
-
+      <CardContent>
+        <br />
+        <Typography variant="h6" component="h2">
+          Co-Founder & Partner
+        </Typography>
+        <Typography variant="h6" component="h2" fontWeight={"800"}>
+          Taylor Quill
+        </Typography>
+        <br />
+        <br />
+        <br />
+        <Typography variant="subtitle" component="p">
+          info@mysite.com
+        </Typography>
+        <br />
+        <Typography variant="body3" component="p">
+          123-456-7890
+        </Typography>
       </CardContent>
-      <CardMedia component="img" image={image3} />
     </Card>
   );
 }
 
-
-
-function TeamMember4() {
-  return (
-    <Card
-      sx={{
-        position: 'relative',
-      }}
-    >
-      <CardContent
-        sx={{
-          position: 'absolute',
-          bottom: "-9%",
-          left: "20%",
-          width: '100',
-          height: '30',
-          backgroundColor: 'white',
-          
-          // display: "flex",
-          // flexDirection: "column",
-          // justifyContent: "center",
-          // alignItems: "center",
-        }}
-      >
-        <Typography variant="h4" marginBottom={"10%"}>Molaen Neak</Typography>
-
-      </CardContent>
-      <CardMedia component="img" image={image4} />
-    </Card>
-  );
-}
-
-
-
-function TeamMember5() {
-  return (
-    <Card
-      sx={{
-        position: 'relative',
-      }}
-    >
-      <CardContent
-        sx={{
-          position: 'absolute',
-          bottom: "-9%",
-          left: "20%",
-          width: '100',
-          height: '30',
-          backgroundColor: 'white',
-          
-          // display: "flex",
-          // flexDirection: "column",
-          // justifyContent: "center",
-          // alignItems: "center",
-        }}
-      >
-        <Typography variant="h4" marginBottom={"10%"}>Molaen Neak</Typography>
-
-      </CardContent>
-      <CardMedia component="img" image={image1} />
-    </Card>
-  );
-}
 
 
